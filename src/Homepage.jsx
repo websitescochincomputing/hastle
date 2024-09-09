eb1c2f6c2ceaa61fc17885629585563e0ae5ac1b
Muneer A <137588707+Muneermadprit@users.noreply.github.com>
2024-09-09 16:06:48 +0530

    Update Homepage.jsx

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Navbar/hero';
 import Product from './components/Navbar/about';
-import TopProducts from './components/Navbar/TopProducts';
+import TopProducts from './components/Navbar/topproducts';
 import Banner from './components/Navbar/bannner';
 import Subscription from './components/Navbar/Subscription';
 import Testimonial from './components/Navbar/Testimonial';
